f186fec3eb875840d5bbe4ad8f7e6d971767cfd9
delightful-source-3050 <joshmrallen@gmail.com>
2020-02-24 19:35:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,14 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  identification = {
+    :id => 445987
+  }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  
 end
 
 def read_from_hash(hash, key)
